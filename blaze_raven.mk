0d73f22ddbf8a4ff26175770b3b4c3249ab4a169
nhuuhuy634 <daxua1234567@gmail.com>
2023-11-12 22:48:54 +0700

    First setup for raviole

---
+++ blaze_raven.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common blaze stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Blaze Unofficial Stuff
+BLAZE_BUILD_TYPE := UNOFFICIAL
+BLAZE_MAINTAINER := 26.pluvieux
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_BLUR := true
+EXTRA_UDFPS_ANIMATIONS := true
+WITH_GMS := true
+# Inherit device configuration
+$(call inherit-product, device/google/raviole/aosp_raven.mk)
+$(call inherit-product, device/google/gs101/lineage_common.mk)
+
+include device/google/raviole/raven/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 6 Pro
+PRODUCT_NAME := blaze_raven
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=raven \
+    PRIVATE_BUILD_DESC="raven-user 14 UP1A.231105.003 11010452 release-keys"
+
+BUILD_FINGERPRINT := google/raven/raven:14/UP1A.231105.003/11010452:user/release-keys
+
+$(call inherit-product, vendor/google/raven/raven-vendor.mk)
